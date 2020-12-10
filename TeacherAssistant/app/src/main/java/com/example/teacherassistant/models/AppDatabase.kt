b65e8e2b5913ef764f4c4bbd0da79ebaab49d003
Wojtek <wojtek.kajstura@gmail.com>
2020-12-10 16:31:50 +0100

    implemented db model

--- TeacherAssistant/app/src/main/java/com/example/teacherassistant/models/AppDatabase.kt
+++ TeacherAssistant/app/src/main/java/com/example/teacherassistant/models/AppDatabase.kt
@@ -1,7 +1,43 @@
 package com.example.teacherassistant.models
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
+@Database(
+    entities = [Student::class,
+                Course::class],
+    version = 1,
+    exportSchema = false)
 abstract class AppDatabase:RoomDatabase() {
 
+    abstract fun CourseDao():CourseDao
+    abstract fun StudentDao():StudentDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE:AppDatabase?=null
+
+        fun getDatabase(context:Context):AppDatabase{
+            val tempInstance = INSTANCE
+
+            if (tempInstance != null){
+                return tempInstance
+            }
+            else{
+                synchronized(this){
+                    val instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDatabase::class.java,
+                        "app_database"
+                    ).build()
+                    INSTANCE = instance
+                    return instance
+                }
+            }
+
+        }
+
+    }
 }
