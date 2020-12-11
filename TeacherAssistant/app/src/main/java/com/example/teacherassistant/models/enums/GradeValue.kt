1ba305e34079eb0d820269ed7a3623f36ee14618
Wojtek <wojtek.kajstura@gmail.com>
2020-12-11 13:11:18 +0100

    created grade enum

---
+++ TeacherAssistant/app/src/main/java/com/example/teacherassistant/models/enums/GradeValue.kt
@@ -0,0 +1,6 @@
+package com.example.teacherassistant.models.enums
+
+enum class GradeValue(val gradeName:String,val gradeValue:Double) {
+    Niedostateczny("Niedostateczny",2.0),
+    BardzoDobry("Bardzo dobry",5.0)
+}
