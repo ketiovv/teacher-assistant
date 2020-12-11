fd82ac95747676969dda8b9b827e9be2edf3637f
Wojtek <wojtek.kajstura@gmail.com>
2020-12-11 13:10:29 +0100

    implemented grade entity

--- TeacherAssistant/app/src/main/java/com/example/teacherassistant/models/Grade.kt
+++ TeacherAssistant/app/src/main/java/com/example/teacherassistant/models/Grade.kt
@@ -1,4 +1,14 @@
 package com.example.teacherassistant.models
 
-class Grade {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.teacherassistant.models.enums.GradeValue
+import java.util.*
+
+@Entity(tableName = "grade_table")
+data class Grade(@PrimaryKey(autoGenerate = true)
+                 val id:Int,
+                 val studentCourseId:Int,
+                 val grade: GradeValue,
+                 val note: String,
+                 val date: Date)
