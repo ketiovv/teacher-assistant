9e9563ca39427667d2ef074afda64d1bb7384b6e
Wojtek <wojtek.kajstura@gmail.com>
2020-12-11 01:38:46 +0100

    implemented course fragment

--- TeacherAssistant/app/src/main/java/com/example/teacherassistant/views/CourseListFragment.kt
+++ TeacherAssistant/app/src/main/java/com/example/teacherassistant/views/CourseListFragment.kt
@@ -5,12 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.teacherassistant.R
+import com.example.teacherassistant.adapters.CourseListAdapter
+import com.example.teacherassistant.viewmodels.CourseListViewModel
+import kotlinx.android.synthetic.main.fragment_course_list.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+private lateinit var courseListViewModel: CourseListViewModel
+
 
 /**
  * A simple [Fragment] subclass.
@@ -21,6 +29,11 @@ class CourseListFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    // --- mine code ---
+    private lateinit var courseListAdapter: CourseListAdapter
+    private lateinit var viewManager: RecyclerView.LayoutManager
+    // --- --- --- --- ---
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,8 +47,31 @@ class CourseListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // --- mine code ---
+
+        courseListViewModel = ViewModelProvider(requireActivity())
+            .get(CourseListViewModel::class.java)
+        viewManager = LinearLayoutManager(context)
+        courseListAdapter = CourseListAdapter(courseListViewModel.courses)
+
+        courseListViewModel.courses.observe(viewLifecycleOwner,{
+            courseListAdapter.notifyDataSetChanged()
+        })
+
+        // --- --- --- --- ---
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_course_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerViewCourses.apply{
+            adapter = courseListAdapter
+            layoutManager = viewManager
+        }
     }
 
     companion object {
