5ff5895a69b9e8bfd3c98aa004c1a4025c6eb049
Wojtek <wojtek.kajstura@gmail.com>
2020-12-11 13:10:40 +0100

    implemented grade dao

--- TeacherAssistant/app/src/main/java/com/example/teacherassistant/models/GradeDao.kt
+++ TeacherAssistant/app/src/main/java/com/example/teacherassistant/models/GradeDao.kt
@@ -1,4 +1,24 @@
 package com.example.teacherassistant.models
 
-class GradeDao {
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface GradeDao {
+    @Query("SELECT * FROM COURSE_TABLE")
+    fun getAll(): LiveData<List<Grade>>
+
+    @Query("SELECT * FROM COURSE_TABLE WHERE ID=:studentId")
+    fun getAllByStudent(studentId:Int): LiveData<List<Grade>>
+
+    @Insert
+    suspend fun insert(grade: Grade)
+
+    @Delete
+    suspend fun delete(grade: Grade)
+
+    // TODO: @Update
 }
