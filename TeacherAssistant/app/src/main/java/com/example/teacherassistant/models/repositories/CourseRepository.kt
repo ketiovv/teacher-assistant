8cae3c8072922f59f600537b150f66db4c4f0adb
Wojtek <wojtek.kajstura@gmail.com>
2020-12-11 01:37:58 +0100

    implemented course repository

--- TeacherAssistant/app/src/main/java/com/example/teacherassistant/models/repositories/CourseRepository.kt
+++ TeacherAssistant/app/src/main/java/com/example/teacherassistant/models/repositories/CourseRepository.kt
@@ -1,4 +1,12 @@
 package com.example.teacherassistant.models.repositories
 
-class CourseRepository {
+import androidx.lifecycle.LiveData
+import com.example.teacherassistant.models.Course
+import com.example.teacherassistant.models.CourseDao
+
+class CourseRepository(private val courseDao: CourseDao) {
+    val readAll:LiveData<List<Course>> = courseDao.getAll()
+
+    suspend fun add(course: Course) = courseDao.insert(course)
+    suspend fun delete(course: Course) = courseDao.delete(course)
 }
