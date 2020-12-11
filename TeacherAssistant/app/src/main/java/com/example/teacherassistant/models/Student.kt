d1779ae1d3f5419135fc0db00ad6964c603e2d47
Wojtek <wojtek.kajstura@gmail.com>
2020-12-11 17:26:59 +0100

    made student parcelable

--- TeacherAssistant/app/src/main/java/com/example/teacherassistant/models/Student.kt
+++ TeacherAssistant/app/src/main/java/com/example/teacherassistant/models/Student.kt
@@ -1,5 +1,7 @@
 package com.example.teacherassistant.models
 
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -7,4 +9,30 @@ import androidx.room.PrimaryKey
 data class Student(@PrimaryKey(autoGenerate = true)
                    val id: Int,
                    var firstName:String,
-                   var lastName:String)
+                   var lastName:String): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString()!!,
+        parcel.readString()!!
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(firstName)
+        parcel.writeString(lastName)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Student> {
+        override fun createFromParcel(parcel: Parcel): Student {
+            return Student(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Student?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
