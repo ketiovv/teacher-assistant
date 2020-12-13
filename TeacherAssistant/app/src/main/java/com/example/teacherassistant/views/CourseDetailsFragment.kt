0b6aad900da065793b11263d0b34d5af28083512
Wojtek <wojtek.kajstura@gmail.com>
2020-12-13 18:35:35 +0100

    implemented course details fragment for showing students in course

--- TeacherAssistant/app/src/main/java/com/example/teacherassistant/views/CourseDetailsFragment.kt
+++ TeacherAssistant/app/src/main/java/com/example/teacherassistant/views/CourseDetailsFragment.kt
@@ -5,8 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.teacherassistant.R
-import com.example.teacherassistant.models.Course
+import com.example.teacherassistant.adapters.CourseStudentsAdapter
+import com.example.teacherassistant.models.entities.Course
+import com.example.teacherassistant.viewmodels.StudentViewModel
 import kotlinx.android.synthetic.main.fragment_course_details.*
 
 private const val ARG_COURSE = "course"
@@ -14,6 +19,11 @@ private const val ARG_COURSE = "course"
 
 class CourseDetailsFragment : Fragment() {
     private lateinit var course: Course
+
+    private lateinit var viewManager:RecyclerView.LayoutManager
+    private lateinit var courseStudentsAdapter: CourseStudentsAdapter
+
+    private lateinit var studentViewModel:StudentViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +36,17 @@ class CourseDetailsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        studentViewModel = ViewModelProvider(requireActivity())
+            .get(StudentViewModel::class.java)
+
+        viewManager = LinearLayoutManager(context)
+        courseStudentsAdapter = CourseStudentsAdapter(studentViewModel.students)
+
+        studentViewModel.setCourseId(course.id)
+        studentViewModel.students.observe(viewLifecycleOwner,{
+            courseStudentsAdapter.notifyDataSetChanged()
+        })
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_course_details, container, false)
     }
@@ -34,6 +55,11 @@ class CourseDetailsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         textViewCourseNameLabel.text = course.name
+
+        recyclerViewCourseStudents.apply{
+            layoutManager = viewManager
+            adapter = courseStudentsAdapter
+        }
     }
 
     companion object {
